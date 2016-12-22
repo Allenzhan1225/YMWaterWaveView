4a87fbe521bba166f9ca639151a51a903db3f9b5
Allenzhan1225 <1042748558@qq.com>
2016-12-22 18:43:36 +0800

    commit changes YMWaterWaveView

---
+++ YMWaterWaveView/YMWaterWaveView/YMWaveProgressView.h
@@ -0,0 +1,13 @@
+//
+//  YMWaveProgressView.h
+//  YMWaterWaveView
+//
+//  Created by Jack on 2016/12/22.
+//  Copyright © 2016年 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YMWaveProgressView : UIView
+
+@end
---
+++ YMWaterWaveView/YMWaterWaveView/YMWaveProgressView.m
@@ -0,0 +1,21 @@
+//
+//  YMWaveProgressView.m
+//  YMWaterWaveView
+//
+//  Created by Jack on 2016/12/22.
+//  Copyright © 2016年 Jack. All rights reserved.
+//
+
+#import "YMWaveProgressView.h"
+
+@implementation YMWaveProgressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
